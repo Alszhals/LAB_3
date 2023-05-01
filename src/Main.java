3ec3f5a4b453e13951cc0dad42cab72d3ecf3cc0
LAB1 ADS <asek05052005@gmail.com>
2023-05-01 23:55:38 +0600

    :/

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         MyArrayListStack s=new MyArrayListStack(3);
+        MyArrayListQueue q= new MyArrayListQueue(3);
     }
 }
