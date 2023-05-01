a524538656ad7968bce3358f0175ca0fa41ac30d
LAB1 ADS <asek05052005@gmail.com>
2023-05-01 23:21:04 +0600

    :/

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -20,4 +20,30 @@ public class MyArrayListQueue {
     public boolean isFull() {
         return (size() == capacity);
     }
+    public int dequeue(){
+        if(isEmpty()){
+            System.out.println("Underflow\nProgram Terminated");
+            System.exit(-1);
+        }
+        int x = arr[front];
+        System.out.println("Removing " + x);
+        front = (front + 1) % capacity;
+        count--;
+        return x;
+    }
+    public void enqueue(int item)
+    {
+
+        if (isFull())
+        {
+            System.out.println("Overflow\nProgram Terminated");
+            System.exit(-1);
+        }
+
+        System.out.println("Inserting " + item);
+
+        rear = (rear + 1) % capacity;
+        arr[rear] = item;
+        count++;
+    }
 }
